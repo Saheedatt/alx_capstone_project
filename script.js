285f55002bb5047759c896ffc156fade3e3d0779
saheedatt <afolabisaheedat1@gmail.com>
2023-10-18 12:37:43 +0100

    Switched to emailjs & updated functions

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
-//for calling multiple events
+// for calling multiple events
 const addEventOnElements = function (elements, eventType, callback) {
-    for (let i = 0; i < elements.length; i++) {
-      elements[i].addEventListener(eventType, callback);
-    }
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
   }
-//for our mobile navbar
+};
+// for our mobile navbar
 const primaryNav = document.querySelector('.primary-nav');
 const navToggler = document.querySelector('.mobile-nav-toggle');
+const navLinks = document.querySelectorAll('.primary-nav a');
 
-navToggler.addEventListener('click', ()=>{
-    const visibility = primaryNav.getAttribute('data-visible')
-
-    if (visibility === "false"){
-        primaryNav.setAttribute("data-visible", true);
-        navToggler.setAttribute('aria-expanded', true)
-    }else if(visibility === "true"){
-        primaryNav.setAttribute("data-visible", false);
-        navToggler.setAttribute('aria-expanded', false);
-    }
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    primaryNav.setAttribute('data-visible', 'false');
+    navToggler.setAttribute('aria-expanded', 'false');
+  });
 });
 
-const header = document.querySelector("[data-header]");
+navToggler.addEventListener('click', () => {
+  const visibility = primaryNav.getAttribute('data-visible');
+
+  if (visibility === 'false') {
+    primaryNav.setAttribute('data-visible', true);
+    navToggler.setAttribute('aria-expanded', true);
+  } else if (visibility === 'true') {
+    primaryNav.setAttribute('data-visible', false);
+    navToggler.setAttribute('aria-expanded', false);
+  }
+});
+
+const header = document.querySelector('[data-header]');
 const activeElementOnScroll = function () {
   if (window.scrollY > 50) {
-    header.classList.add("active");
+    header.classList.add('active');
   } else {
-    header.classList.remove("active");
+    header.classList.remove('active');
   }
-}
-window.addEventListener("scroll", activeElementOnScroll);
+};
+window.addEventListener('scroll', activeElementOnScroll);
 
-/*Hero text animation*/
-const letterBoxes = document.querySelectorAll("[data-animated-word]");
+/* Hero text animation */
+const letterBoxes = document.querySelectorAll('[data-animated-word]');
 
 let activeLetterBoxIndex = 0;
 let lastActiveLetterBoxIndex = 0;
@@ -42,11 +50,11 @@ const setAnimationDelay = (element, delay) => {
 };
 
 const createLetterSpan = (letter, delay, isActive) => {
-  const span = document.createElement("span");
+  const span = document.createElement('span');
   setAnimationDelay(span, delay);
   span.textContent = letter;
-  span.classList.add(isActive ? "in" : "out");
-  if (letter === " ") span.classList.add("space");
+  span.classList.add(isActive ? 'in' : 'out');
+  if (letter === ' ') span.classList.add('space');
   return span;
 };
 
@@ -54,7 +62,7 @@ const setLetterEffect = () => {
   for (let i = 0; i < letterBoxes.length; i++) {
     let letterAnimationDelay = 0;
     const letters = letterBoxes[i].textContent.trim();
-    letterBoxes[i].textContent = "";
+    letterBoxes[i].textContent = '';
 
     for (let j = 0; j < letters.length; j++) {
       const isActiveLetterBox = i === activeLetterBoxIndex;
@@ -68,7 +76,7 @@ const setLetterEffect = () => {
       totalLetterBoxDelay = Number(letterAnimationDelay.toFixed(2));
     }
 
-    letterBoxes[i].classList.toggle("active", i === lastActiveLetterBoxIndex);
+    letterBoxes[i].classList.toggle('active', i === lastActiveLetterBoxIndex);
   }
 
   setTimeout(() => {
@@ -78,10 +86,10 @@ const setLetterEffect = () => {
   }, (totalLetterBoxDelay * 1000) + 3000);
 };
 
-window.addEventListener("load", setLetterEffect);
+window.addEventListener('load', setLetterEffect);
 
-/*Scrolling*/
-const revealElements = document.querySelectorAll("[data-reveal]");
+/* Scrolling */
+const revealElements = document.querySelectorAll('[data-reveal]');
 
 const isElementInView = (element) => {
   const rect = element.getBoundingClientRect();
@@ -89,7 +97,7 @@ const isElementInView = (element) => {
 };
 
 const revealElement = (element, reveal) => {
-  element.classList.toggle("revealed", reveal);
+  element.classList.toggle('revealed', reveal);
 };
 
 const handleScrollReveal = () => {
@@ -100,16 +108,15 @@ const handleScrollReveal = () => {
 };
 
 const scrollReveal = () => {
-  window.addEventListener("scroll", handleScrollReveal);
+  window.addEventListener('scroll', handleScrollReveal);
   handleScrollReveal();
 };
 
 scrollReveal();
 
-
-const cursor = document.querySelector("[data-cursor]");
-const anchorElements = document.querySelectorAll("a");
-const buttons = document.querySelectorAll("button");
+const cursor = document.querySelector('[data-cursor]');
+const anchorElements = document.querySelectorAll('a');
+const buttons = document.querySelectorAll('button');
 
 const updateCursorPosition = (event) => {
   setTimeout(() => {
@@ -119,11 +126,11 @@ const updateCursorPosition = (event) => {
 };
 
 const addHoverClass = () => {
-  cursor.classList.add("hovered");
+  cursor.classList.add('hovered');
 };
 
 const removeHoverClass = () => {
-  cursor.classList.remove("hovered");
+  cursor.classList.remove('hovered');
 };
 
 const addEventListeners = (elements, eventType, eventHandler) => {
@@ -132,22 +139,21 @@ const addEventListeners = (elements, eventType, eventHandler) => {
   });
 };
 
-document.body.addEventListener("mousemove", updateCursorPosition);
-addEventListeners(anchorElements, "mouseover", addHoverClass);
-addEventListeners(anchorElements, "mouseout", removeHoverClass);
-addEventListeners(buttons, "mouseover", addHoverClass);
-addEventListeners(buttons, "mouseout", removeHoverClass);
+document.body.addEventListener('mousemove', updateCursorPosition);
+addEventListeners(anchorElements, 'mouseover', addHoverClass);
+addEventListeners(anchorElements, 'mouseout', removeHoverClass);
+addEventListeners(buttons, 'mouseover', addHoverClass);
+addEventListeners(buttons, 'mouseout', removeHoverClass);
 
-document.body.addEventListener("mouseout", () => {
-  cursor.classList.add("disabled");
+document.body.addEventListener('mouseout', () => {
+  cursor.classList.add('disabled');
 });
 
-document.body.addEventListener("mouseover", () => {
-  cursor.classList.remove("disabled");
+document.body.addEventListener('mouseover', () => {
+  cursor.classList.remove('disabled');
 });
 
-
-const backTopBtn = document.querySelector("[data-back-top-btn]");
+const backTopBtn = document.querySelector('[data-back-top-btn]');
 
 const calculateScrollPercent = () => {
   const bodyHeight = document.body.scrollHeight;
@@ -161,9 +167,9 @@ const updateBackToTopButton = () => {
   backTopBtn.textContent = `${totalScrollPercent.toFixed(0)}%`;
 
   if (totalScrollPercent > 5) {
-    backTopBtn.classList.add("show");
+    backTopBtn.classList.add('show');
   } else {
-    backTopBtn.classList.remove("show");
+    backTopBtn.classList.remove('show');
   }
 };
 
@@ -171,14 +177,36 @@ const handleScroll = () => {
   updateBackToTopButton();
 };
 
-window.addEventListener("scroll", handleScroll);
+window.addEventListener('scroll', handleScroll);
 
+// function to validate my form
+function validateForm () {
+  const nameInput = document.getElementById('name');
+  const name = nameInput.value.trim();
+  const nameError = document.getElementById('name-error');
 
-function validateForm() {
+  const messageInput = document.getElementById('message');
+  const message = messageInput.value.trim();
+  const messageError = document.getElementById('message-error');
+
   const emailInput = document.getElementById('email');
   const email = emailInput.value.trim();
   const emailError = document.getElementById('email-error');
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (name === '') {
+    nameError.textContent = 'Name is required.';
+    nameInput.focus();
+    return false;
+  }
+  nameError.textContent = '';
+
+  if (message === '') {
+    messageError.textContent = 'Message is required.';
+    messageInput.focus();
+    return false;
+  }
+  messageError.textContent = '';
 
   if (email === '' || !emailRegex.test(email)) {
     emailError.textContent = 'Please enter a valid email address.';
@@ -186,22 +214,46 @@ function validateForm() {
     return false;
   }
   emailError.textContent = '';
-
   return true;
 }
 
+// using emailjs to send email for visitors when they fill my contact form
+function sendMail () {
+  const serviceID = 'service_avuacoe';
+  const templateID = 'template_m7zb8mv';
 
-function sendEmail(){
-  Email.send({
-    secureToken: "c19165e2-c3d7-4fb7-a703-589ef2c3ca50",
+  const name = document.querySelector('#name').value;
+  const mail = document.querySelector('#email').value;
+  const message = document.querySelector('#message').value;
 
-    To : 'afolabisaheedat1@gmail.com',
-    From : document.getElementById('email').value,
-    Subject : "Contact Form Enquiry",
-    Body : "Name: " + document.getElementById('name').value
-          + "<br> Email: " + document.getElementById('email').value
-          + "<br> Message: " + document.getElementById('message').value
-}).then(
-  message => alert("Message Sent Successfully")
-);
+  const params = {
+    sendername: name,
+    senderemail: mail,
+    message
+  };
+
+  emailjs.send(serviceID, templateID, params)
+    .then(function (response) {
+      console.log('SUCCESS!', response.status, response.text);
+      // Clears the form and show success message
+      document.getElementById('success-message').innerText = 'Form successfully submitted.';
+      document.getElementById('success-message').style.display = 'block';
+      document.getElementById('contact-form').reset();
+    }, function (error) {
+      console.log('FAILED...', error);
+    });
+
+  // Prevents the form from submitting normally
+  return false;
+}
+
+// To ensure that my form is validated first before submission
+function validateAndSendMail () {
+  const isFormValid = validateForm();
+
+  if (isFormValid) {
+    sendMail();
+  }
+
+  return false;
 }
